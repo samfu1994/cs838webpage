31ce76f10337d38c1ec26c35e22f94b4cbf1e9da
fuhao <gowithhao@yahoo.com>
2017-03-12 22:43:22 -0500

    update from local repo

--- code/extra_neg.py
+++ code/extra_neg.py
@@ -1,14 +1,17 @@
 import random
 import re
+from os import listdir
+from os.path import isfile, join
+
 
 
 def extra_negative_instances(inFile, num):
   negArray = []
   file = open(inFile)
   text = str(file.read())
-  p1 = '(<n>(.*?)<n>)'
-  p2 = '(<p1>(.*?)<p1>)'
-  p3 = '(<p2>(.*?)<p2>)'
+  p1 = '<n (.*?) n>'
+  p2 = '<p1 (.*?) p1>'
+  p3 = '<p2 (.*?) p2>'
   text = re.sub(p1+'|'+p2+'|'+p3, '', text)
   text = re.sub('[0-9,.;:?!\[\]\r\n\t()\"]', ' ', text)
   array = text.split()
@@ -22,4 +25,23 @@ def extra_negative_instances(inFile, num):
     if index > 0:
       st = array[index - 1] + ' ' + st
     negArray.append(st)
-  print negArray
+    return negArray
+    
+def main():
+  mypath = "/Users/fuhao/Development/cs838webpage/textFile/"
+  files = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f[-4:] == ".txt"]
+  train_files = files[:200]
+  test_files = files[200:]
+  data = []
+  word = ""
+  countp = 0
+  countn = 0
+  for f in files:
+    c1, c2 = extra_negative_instances(mypath + f, 0)
+    countp += c1
+    countn += c2
+  print countp
+  print countn
+
+if __name__ == "__main__":
+  main()
